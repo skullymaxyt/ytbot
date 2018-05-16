95350302b3cca1b25ef21d7f452f87cc6fa61ee6
skullymaxyt <39335737+skullymaxyt@users.noreply.github.com>
2018-05-16 13:45:48 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+
+client.on("ready", () => {
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildCreate", guild => {
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+function altgen() {
+  var rand = ['user:155',
+  'asgasg:123',
+  '15132:sfasf'];
+
+  return rand[Math.floor(Math.random()*rand.length)];
+}
+
+client.on("message", async message => {
+
+  if(message.author.bot) return;
+
+  if(message.content.indexOf(config.prefix) !== 0) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+if(command === "generate") {
+  message.channel.send("**Generating alts...**");
+    message.author.send(altgen());
+  }
+});
+
+  client.login(config.token);
